e1e79a1480fb394c8dbd240115eeac5f7bb83500
Joseph Ng <joseph_sieang@live.com>
2022-12-28 16:07:37 +0800

    Prevent tree-shaking can execute correctly to reduce initial bundle size

--- projects/poc-lib/src/public-api.ts
+++ projects/poc-lib/src/public-api.ts
@@ -2,10 +2,6 @@
  * Public API Surface of poc-lib
  */
 
-export * from './lib/library-a/library-a.module';
-export * from './lib/library-b/library-b.module';
-export * from './lib/library-c/library-c.module';
-
-export * from './lib/library-a/a/a.component';
-export * from './lib/library-b/b/b.component';
-export * from './lib/library-c/c/c.component';
+export * from './lib/library-a';
+export * from './lib/library-b';
+export * from './lib/library-c';
